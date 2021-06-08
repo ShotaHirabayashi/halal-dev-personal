bbb977a7344b5842462a1c6e0166992fef9322f2
ShotaHirabayashi <bys.888812@gmail.com>
2021-06-09 00:49:02 +0900

    mosque grocery 詳細ページ追加

---
+++ pages/grocery/detail/[store_id].js
@@ -0,0 +1,38 @@
+
+import Layout from '../../../components/Layout'
+import {getAllDetailData,getGroceryDetailData,getAllGroceryDetailIds} from '../../../lib/groceryDetail'
+
+
+const GroceryDetail = ({grocery}) => {
+    return (
+        <Layout title={grocery.store_name}>
+            <p>{grocery.store_name}</p>
+            <p>{grocery.address}</p>
+            <p>{grocery.main_msg}</p>
+            <img src={grocery.main_img} height={100} />
+        </Layout>
+    )
+}
+
+export default GroceryDetail
+
+// ビルド時に取得
+export async function getStaticPaths(){
+    const paths = await getAllGroceryDetailIds();
+
+    return {
+        paths,
+        // 101番の場合は404
+        fallback:false,
+    }
+}
+
+
+export async function getStaticProps({params}){
+    const {grocery:grocery} = await getGroceryDetailData(params.store_id);
+    return {
+        props: {
+            grocery
+        }
+    }
+}
---
+++ pages/mosque/detail/[store_id].js
@@ -0,0 +1,38 @@
+
+import Layout from '../../../components/Layout'
+import {getMosqueDetailData,getAllMosqueDetailIds} from '../../../lib/mosqueDetail'
+
+
+const MosqueDetail = ({mosque}) => {
+    return (
+        <Layout title={mosque.store_name}>
+            <p>{mosque.store_name}</p>
+            <p>{mosque.address}</p>
+            <p>{mosque.main_msg}</p>
+            <img src={mosque.main_img} height={100} />
+        </Layout>
+    )
+}
+
+export default MosqueDetail
+
+// ビルド時に取得
+export async function getStaticPaths(){
+    const paths = await getAllMosqueDetailIds();
+
+    return {
+        paths,
+        // 101番の場合は404
+        fallback:false,
+    }
+}
+
+
+export async function getStaticProps({params}){
+    const {mosque:mosque} = await getMosqueDetailData(params.store_id);
+    return {
+        props: {
+            mosque
+        }
+    }
+}
