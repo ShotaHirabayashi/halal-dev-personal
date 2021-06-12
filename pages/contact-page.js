07511e771468c56556fdca6bb50616b14ac9d950
ShotaHirabayashi <bys.888812@gmail.com>
2021-06-12 10:02:56 +0900

    image delete

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,15 +1,8 @@
 import Layout from '../components/Layout'
-import Image from 'next/image'
 const Contact= () => {
     return (
         <Layout title="contact">
             Contact
-            <Image 
-            className="rounded-full"
-            src="/avatar.jpg"
-            width={60}
-            height={60}
-            alt="avatart" />
         </Layout>
     )
 }
