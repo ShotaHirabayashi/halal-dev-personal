566fe7adec20fba9d001ea42cc176c2c83aef8c4
ShotaHirabayashi <bys.888812@gmail.com>
2021-06-14 18:18:29 +0900

    test

--- components/StoreList.js
+++ components/StoreList.js
@@ -4,9 +4,9 @@ import Link from 'next/link'
 const StoreListComp = ({store,category}) => {
     return (
         <div>
-            <span>{store.store_name}</span>{":"}
-            <Link href={`/${category}/detail/${store.store_id}`}>
-            <span className="cursor-pointer text-blue-500 hover:bg-gray-500">{store.address}</span>
+            <span>{store.store_name && store.store_name}</span>{":"}
+            <Link href={`/${category}/detail/${store.store_name && store.store_id}`}>
+            <span className="cursor-pointer text-blue-500 hover:bg-gray-500">{store.address && store.address}</span>
             </Link>
         </div>
     )
