325803ca5c15fe8a53aad11fe9baeb1310cc5ca7
ShotaHirabayashi <bys.888812@gmail.com>
2021-06-15 00:14:20 +0900

    go

--- pages/grocery/detail/[store_id].js
+++ pages/grocery/detail/[store_id].js
@@ -6,11 +6,11 @@ import {useRouter} from 'next/router'
 
 const GroceryDetail = ({grocery}) => {
     return (
-        <Layout title={grocery.grocery && grocery.grocery.store_name}>
-            <p>{grocery.grocery && grocery.grocery.store_name}</p>
-            <p>{grocery.grocery && grocery.grocery.address}</p>
-            <p>{grocery.grocery && grocery.grocery.main_msg}</p>
-            <img src={grocery.grocery && grocery.grocery.main_img} height={100} />
+        <Layout title={grocery && grocery.grocery.store_name}>
+            <p>{grocery && grocery.grocery.store_name}</p>
+            <p>{grocery && grocery.grocery.address}</p>
+            <p>{grocery && grocery.grocery.main_msg}</p>
+            <img src={grocery && grocery.grocery.main_img} height={100} />
         </Layout>
     )
 }
